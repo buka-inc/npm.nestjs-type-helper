6de3460a6b1775e158845c4ed8fec24af2f4a78b
Val.istar.Guo <val.istar.guo@gmail.com>
2025-03-20 02:38:23 +0800

    feat: update Page.from to return EntityDTO items

--- src/pagination/page.ts
+++ src/pagination/page.ts
@@ -4,6 +4,7 @@ import { Type as ClassType } from 'class-transformer'
 import { ValidateNested } from 'class-validator'
 import { Pagination } from './pagination.js'
 import { PageQuery } from './page-query.js'
+import { EntityDTO } from '@mikro-orm/core'
 
 
 export function Page<T>(classRef: Type<T>): Type<{ items: T[]; pagination: Pagination }> {
@@ -31,7 +32,10 @@ export function Page<T>(classRef: Type<T>): Type<{ items: T[]; pagination: Pagin
 }
 
 
-Page.from = <T>(items: T[], total: number, pageQuery: PageQuery) => ({
-  items,
+Page.from = <T>(items: T[], total: number, pageQuery: PageQuery): { items: EntityDTO<T>[]; pagination: Pagination } => ({
+  items: items.map((item) => {
+    if (item && typeof item['toObject'] === 'function') return item['toObject']() as EntityDTO<T>
+    return item as EntityDTO<T>
+  }),
   pagination: Pagination.from(total, pageQuery),
 })
