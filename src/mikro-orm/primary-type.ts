2a06d0844f7527f797ad07b3294ee4a77c621694
val.istar.guo <val.istar.guo@gmail.com>
2025-03-28 16:38:41 +0800

    fix: duplicate name of EntityDtoType

--- src/mikro-orm/primary-type.ts
+++ src/mikro-orm/primary-type.ts
@@ -1,16 +1,28 @@
 import { Type } from '@nestjs/common'
+
 import { IPrimaryDto } from './types/primary-dto'
+import * as SwaggerUtils from '~/utils/nestjs-swagger-utils'
 import * as MikroOrmUtils from '~/utils/mikro-orm-utils'
-import { PickType } from '@nestjs/swagger'
+import { inheritTransformationMetadata, inheritValidationMetadata } from '@nestjs/mapped-types'
 
 
 export function PrimaryType<T extends object>(classRef: Type<T>): Type<IPrimaryDto<T>> | undefined {
   const properties = MikroOrmUtils.getMetadata(classRef)
 
   const primaryProperties = properties.filter((prop) => prop.primary)
-    .map((prop) => prop.name)
+    .map((prop) => prop.name as string)
 
   if (!primaryProperties.length) return
 
-  return PickType(classRef, primaryProperties) as unknown as Type<IPrimaryDto<T>>
+  const clsName = `${classRef.name}PrimaryClass`
+
+  const cls = new Function(`
+    return class ${clsName} {}
+  `)() as Type<IPrimaryDto<T>>
+
+  SwaggerUtils.cloneMetadata(cls, classRef, primaryProperties)
+  inheritValidationMetadata(classRef, cls, (key) => primaryProperties.includes(key))
+  inheritTransformationMetadata(classRef, cls, (key) => primaryProperties.includes(key))
+
+  return cls
 }
